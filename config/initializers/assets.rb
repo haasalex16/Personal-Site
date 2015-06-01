87595467c544480fa16984e174d5d44ae02f2b3d
Alex Haas <alex.carl.haas@gmail.com>
2015-06-01 00:12:45 -0400

    Update Asset Path for js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( jquery.poptrox.min.js )
+Rails.application.config.assets.precompile += %w( skel.min.js )
+Rails.application.config.assets.precompile += %w( util.js )
+Rails.application.config.assets.precompile += %w( ie/respond.min.js )
+Rails.application.config.assets.precompile += %w( main.js )
